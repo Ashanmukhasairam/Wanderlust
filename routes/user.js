f68429e1c3ec33f25724a1be626c739592c38ce0
Ashanmukhasairam <shanmukhaagnihotram@gmail,com>
2025-01-08 07:36:56 +0530

    user model created and authentication is started to the website

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../Models/user.js");
 
 router.get("/signup", (req,res) =>{
     res.render("users/signup.ejs");
 });
+
+router.post("/signup",async(req,res)=>{
+    let {username,email,password} = req.body;
+    const newUser = new User({username,email});
+    const RegisteredUser = await User.register(newUser,password);
+    req.flash("success","Welcome to Wanderlust");
+    res.redirect("/listings");
+
+})
 
 module.exports = router;
